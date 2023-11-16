3c8fad57e05cd482adaa9fc92c9112a57a0380f5
Nesa02 <nenad.beric00@gmail.com>
2023-11-16 20:31:51 +0100

    Basic navigation added

--- app/src/main/java/com/example/booking_ma_tim21/activities/AccountActivity.java
+++ app/src/main/java/com/example/booking_ma_tim21/activities/AccountActivity.java
@@ -1,19 +1,29 @@
 package com.example.booking_ma_tim21.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.booking_ma_tim21.R;
 import com.example.booking_ma_tim21.util.NavigationSetup;
 
-public class MainActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout main, account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account);
 
         NavigationSetup.setupNavigation(this);
     }
