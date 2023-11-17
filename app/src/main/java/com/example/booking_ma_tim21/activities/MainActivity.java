256eb9151e4372f007136ad4360619428984499f
StefanDjuricaSV35-2021 <104830728+StefanDjuricaSV35-2021@users.noreply.github.com>
2023-11-17 13:54:20 +0100

    Revert "Login/register"

--- app/src/main/java/com/example/booking_ma_tim21/activities/MainActivity.java
+++ app/src/main/java/com/example/booking_ma_tim21/activities/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.booking_ma_tim21.R;
-import com.example.booking_ma_tim21.util.NavigationSetup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        NavigationSetup.setupNavigation(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        NavigationSetup.closeDrawer(findViewById(R.id.drawerLayout));
     }
 }
