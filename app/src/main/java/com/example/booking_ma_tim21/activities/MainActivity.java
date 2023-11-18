ce4cca78133af386cb200de6a5aa4a99b0acd230
Borislav Čelar <bora.celar@gmail.com>
2023-11-18 02:02:11 +0100

    Squashed commit of the following:
    commit 62963e0fd1658f6582753dfbacc487b34b517ffc
    Author: Borislav Čelar <bora.celar@gmail.com>
    Date:   Sat Nov 18 01:54:00 2023 +0100
    
        main page layout addition
    
        added accommodation preview cards, search and filter buttons

--- app/src/main/java/com/example/booking_ma_tim21/activities/MainActivity.java
+++ app/src/main/java/com/example/booking_ma_tim21/activities/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.booking_ma_tim21.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.booking_ma_tim21.R;
+
 import com.example.booking_ma_tim21.util.NavigationSetup;
 
+import com.example.booking_ma_tim21.adapter.PreviewAdapter;
+import com.example.booking_ma_tim21.model.AccommodationPreview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView previewRecycler;
+    PreviewAdapter previewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +28,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         NavigationSetup.setupNavigation(this);
+
+        List<AccommodationPreview> previews=new ArrayList<>();
+        previews.add(new AccommodationPreview("AAA","Beograd","1",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("BBB","Novi Sad","2",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("CCC","Nis","3",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("DDD","Kragujevac","4",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("EEE","Leskovac","5",R.drawable.apt_img));
+        setPreviewRecycler(previews);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         NavigationSetup.closeDrawer(findViewById(R.id.drawerLayout));
+
+
+    }
+
+    private void setPreviewRecycler(List<AccommodationPreview> accommodationPreview){
+
+        previewRecycler = findViewById(R.id.preview_recycler);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
+        previewRecycler.setLayoutManager(layoutManager);
+        previewAdapter= new PreviewAdapter(this,accommodationPreview);
+        previewRecycler.setAdapter(previewAdapter);
+
     }
 }
