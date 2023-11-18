4150dfdb10e07b032aacc7a32d6b35d7c4deab1b
Nesa02 <nenad.beric00@gmail.com>
2023-11-18 17:54:18 +0100

    Account details and change account added

--- app/src/main/java/com/example/booking_ma_tim21/activities/ChangeAccountActivity.java
+++ app/src/main/java/com/example/booking_ma_tim21/activities/ChangeAccountActivity.java
@@ -1,39 +1,28 @@
 package com.example.booking_ma_tim21.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.example.booking_ma_tim21.R;
 import com.example.booking_ma_tim21.util.NavigationSetup;
 
-public class AccountActivity extends AppCompatActivity {
-
-    DrawerLayout drawerLayout;
-    ImageView menu;
-    LinearLayout main, account;
+public class ChangeAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
-
+        setContentView(R.layout.activity_change_account);
         NavigationSetup.setupNavigation(this);
 
-        Button changeAccountBtn = findViewById(R.id.button_change_account);
-        changeAccountBtn.setOnClickListener(new View.OnClickListener() {
+        Button saveChangesBtn = findViewById(R.id.button_save_account_changes);
+        saveChangesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AccountActivity.this, ChangeAccountActivity.class);
+                Intent intent = new Intent(ChangeAccountActivity.this, AccountActivity.class);
                 startActivity(intent);
 
             }
