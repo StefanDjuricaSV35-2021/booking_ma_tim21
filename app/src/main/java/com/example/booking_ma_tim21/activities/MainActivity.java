d3726bee1a4d2c2b159b51c1502d45e8d127a99a
Borislav Čelar <bora.celar@gmail.com>
2023-11-19 14:45:55 +0100

    ui change
    completed search bar, changed accommodation preview card size

--- app/src/main/java/com/example/booking_ma_tim21/activities/MainActivity.java
+++ app/src/main/java/com/example/booking_ma_tim21/activities/MainActivity.java
@@ -4,7 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.LayoutTransition;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.Fade;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.booking_ma_tim21.R;
 
@@ -18,7 +27,6 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-
     RecyclerView previewRecycler;
     PreviewAdapter previewAdapter;
 
@@ -27,24 +35,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializePreviews();
+
         NavigationSetup.setupNavigation(this);
 
-        List<AccommodationPreview> previews=new ArrayList<>();
-        previews.add(new AccommodationPreview("AAA","Beograd","1",R.drawable.apt_img));
-        previews.add(new AccommodationPreview("BBB","Novi Sad","2",R.drawable.apt_img));
-        previews.add(new AccommodationPreview("CCC","Nis","3",R.drawable.apt_img));
-        previews.add(new AccommodationPreview("DDD","Kragujevac","4",R.drawable.apt_img));
-        previews.add(new AccommodationPreview("EEE","Leskovac","5",R.drawable.apt_img));
-        setPreviewRecycler(previews);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         NavigationSetup.closeDrawer(findViewById(R.id.drawerLayout));
-
-
     }
+
+
+    private void initializePreviews(){
+        List<AccommodationPreview> previews=new ArrayList<>();
+
+        previews.add(new AccommodationPreview("AAA","Beograd","1",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("BBB","Novi Sad","2",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("CCC","Nis","3",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("DDD","Kragujevac","4",R.drawable.apt_img));
+        previews.add(new AccommodationPreview("EEE","Leskovac","5",R.drawable.apt_img));
+
+        setPreviewRecycler(previews);
+    }
+
+
+
 
     private void setPreviewRecycler(List<AccommodationPreview> accommodationPreview){
 
